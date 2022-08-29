e8e007bde4dd80e993ede16badd2091e5846b61b
DOMAYA <51879052+Domaya@users.noreply.github.com>
2022-08-29 22:10:40 +0900

    11번 문제
    3,6,9가 두번 씩 들어갈 땐 박수짝짝으로 나오도록

--- KOSA/자바스터디/1주차/No11.java
+++ KOSA/자바스터디/1주차/No11.java
@@ -2,8 +2,12 @@ public class No11{
     public static void main(String []args){
 
         for(int i=1;i<=99;i++){
-            if(i/10==3||i/10==6||i/10==9)
-                System.out.printf("%d 박수 짝\n", i);
+            if(i/10==3||i/10==6||i/10==9) {
+                System.out.printf("%d 박수 짝", i);
+                if(i%10==3||i%10==6||i%10==9)
+                    System.out.print("짝");
+                System.out.println();
+            }
             else if(i%10==3||i%10==6||i%10==9){
                 System.out.printf("%d 박수 짝 \n", i);
             }
