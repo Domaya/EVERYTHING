e506f5c09202130af3d828548678f59287568d85
DOMAYA <51879052+Domaya@users.noreply.github.com>
2022-09-04 17:42:50 +0900

    Create Ex11.java

---
+++ KOSA/자바스터디/2주차/CH3_Array/Ex11.java
@@ -0,0 +1,12 @@
+package CH3_Array;
+
+public class Ex11 {
+    public static void main(String args[]) {
+        int sum = 0;
+
+        for(int i = 0; i < args.length; i++) {
+            sum +=Integer.parseInt(args[i]);
+        }
+        System.out.println(sum / args.length);
+    }
+}
