be7a2abd60326d31b07615ea37f154a79765c904
DOMAYA <51879052+Domaya@users.noreply.github.com>
2022-09-04 22:04:41 +0900

    Create Ex03.java

---
+++ KOSA/자바스터디/2주차/CH4_Class_Object/Ex03.java
@@ -0,0 +1,29 @@
+
+class Song {
+    String title;
+    String artist;
+    int year;
+    String country;
+    //생성자
+    Song(String title, String artist, int year, String country){
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        this.country = country;
+    }
+    //모든 필드를 초기화하는 생성자
+    Song(){
+        this("title", "artist", 1970, "country");
+    }
+    //정보를 출력하는 show메소드
+    public void show(){
+        System.out.println(year+"년 "+country+"국적의 "+artist+"가 부른 "+title);
+    }
+}
+
+public class Ex03 {
+    public static void main(String[] args) {
+        Song song = new Song("Dancing Queen", "ABBA", 1978, "스웨덴");
+        song.show();
+    }
+}
