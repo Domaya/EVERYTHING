df758fc4b8384d5901360ad6dc26d2737166bbfa
DOMAYA <51879052+Domaya@users.noreply.github.com>
2022-09-13 08:56:34 +0900

    Update myEmail.java

--- KOSA/자바스터디/2주차/myEmail.java
+++ KOSA/자바스터디/2주차/myEmail.java
@@ -2,9 +2,10 @@ import java.util.regex.Pattern;
 
 public class myEmail {
     public static void main(String[] args) {
+        String pattern1 = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
         String pattern2 = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
         String str2 = "abc_123@gmail.com";
-        if(Pattern.matches(pattern2, str2)) {
+        if(Pattern.matches(pattern1, str2)) {
             System.out.println("올바른 이메일 형식입니다. ");
         } else {
             System.out.println("올바른 이메일 형식이 아닙니다. ");
